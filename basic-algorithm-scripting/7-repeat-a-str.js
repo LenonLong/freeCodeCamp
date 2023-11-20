8cb914ac0bf9fdb7968a35efb20bcc7c64fb0785
Lenon Long <amronglong@gmail.com>
2023-11-20 09:48:42 -0800

    completed basic algo problem

---
+++ basic-algorithm-scripting/7-repeat-a-str.js
@@ -0,0 +1,16 @@
+/*
+Repeat a String Repeat a String
+Repeat a given string str (first argument) for num times (second argument). 
+Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
+*/
+function repeatStringNumTimes(str, num) {
+    let repeat = '';
+
+    for(let i = 0; i < num; i++) {
+        repeat += str;
+    }
+    return repeat;
+
+  }
+  
+ console.log(repeatStringNumTimes("abc", 3));
